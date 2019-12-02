8aeb7dc21ae8b6cc96ea6431c7f6a6f95bc7661f
zong <1016767058@qq.com>
2019-12-02 17:02:36 +0800

    login page

--- src/main/java/com/zuel/fleamarket/controller/IndexController.java
+++ src/main/java/com/zuel/fleamarket/controller/IndexController.java
@@ -5,7 +5,7 @@ import com.jfinal.core.Controller;
 public class IndexController extends Controller {
 
     public void index() {
-        render("index.jsp");
+        render("/pages/admin/adminIndex.html");
     }
 
     public void test(){
